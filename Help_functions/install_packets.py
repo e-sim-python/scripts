d81a1ded7fb5d307b9f16f53028205ddc982e117
e-sim-python <71469611+e-sim-python@users.noreply.github.com>
2020-09-24 03:34:06 +0300

    Update install_packets.py

--- Help_functions/install_packets.py
+++ Help_functions/install_packets.py
@@ -1,26 +1,36 @@
-def imports():
-    """Try importing most required modules for this library"""
+input("This program should help you install lxml and requests modules, using many different methods. Press Enter to continue.")
+try:
+    from pip._internal import main
+    main(['install', "lxml"])
+    main(['install', "requests"])
+    import lxml
+    import requests
+except:
     try:
-        import requests
-    except ImportError as packet:
-        install([packet.name])
-    try:
+        import pip
+        pip.main(['install', "lxml"])
+        pip.main(['install', "requests"])
         import lxml
-    except ImportError as packet:
-        install([packet.name])
+        import requests
 
-def install(packets):
-    import subprocess
-    for packet in packets:
+    except:
         try:
-            subprocess.call(['pip', 'install', packet.strip()])
-        except Exception as e:
-            print(e)
-            input("Press any key to continue")
-
-
-if __name__ == "__main__":
-    imports()
-    packets = input("Pls write the packets names you want to install (separated by a comma):\n").split(",")
-    install(packets)
-    input("The program is over. You may close this window now.")
+            import subprocess
+            import sys
+            subprocess.check_call([sys.executable, "-m", "pip", "install", "lxml"])
+            subprocess.check_call([sys.executable, "-m", "pip", "install", "requests"])
+            import lxml
+            import requests                
+        except:
+            try:
+                import subprocess
+                subprocess.call(['pip', 'install', "lxml"])
+                subprocess.call(['pip', 'install', "requests"])
+                import lxml
+                import requests   
+            except Exception as e:
+                print(e)
+                print("Sorry, but i couldn't help. search in google for more options (your goal is to install requests and lxml modules")
+                print("Here's a good place to start: https://www.google.com/search?q=Installing+python+module+within+code ")
+                break
+input("Press any key to continue")
