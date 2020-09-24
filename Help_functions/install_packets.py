56405755b8ee7a2c0dcc496362b730067454dcf3
e-sim-python <71469611+e-sim-python@users.noreply.github.com>
2020-09-24 04:15:42 +0300

    Update install_packets.py

--- Help_functions/install_packets.py
+++ Help_functions/install_packets.py
@@ -1,36 +1,48 @@
 input("This program should help you install lxml and requests modules, using many different methods. Press Enter to continue.")
-try:
-    from pip._internal import main
-    main(['install', "lxml"])
-    main(['install', "requests"])
-    import lxml
-    import requests
-except:
-    try:
-        import pip
-        pip.main(['install', "lxml"])
-        pip.main(['install', "requests"])
-        import lxml
-        import requests
 
+
+def method1(package):
+    from pip._internal import main
+    main(['install', package])
+    import requests
+    import lxml
+    
+def method2(package):
+    import pip
+    pip.main(['install', package])
+    import requests
+    import lxml
+
+def method3(package):
+    import subprocess
+    import sys
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+    import requests
+    import lxml
+
+def method4(package):
+    import subprocess
+    subprocess.call(['pip', 'install', package])
+    import requests
+    import lxml
+    
+
+for package in ["lxml", "requests"]:
+    try:
+        method1(package)
     except:
         try:
-            import subprocess
-            import sys
-            subprocess.check_call([sys.executable, "-m", "pip", "install", "lxml"])
-            subprocess.check_call([sys.executable, "-m", "pip", "install", "requests"])
-            import lxml
-            import requests                
+            method2(package)
         except:
             try:
-                import subprocess
-                subprocess.call(['pip', 'install', "lxml"])
-                subprocess.call(['pip', 'install', "requests"])
-                import lxml
-                import requests   
-            except Exception as e:
-                print(e)
-                print("Sorry, but i couldn't help. search in google for more options (your goal is to install requests and lxml modules")
-                print("Here's a good place to start: https://www.google.com/search?q=Installing+python+module+within+code ")
-                break
+                 method3(package)
+            except:
+                try:
+                    method4(package)
+                except Exception as e:
+                    print(e)
+                    print("Sorry, but i couldn't help. search in google for more options (your goal is to install requests and lxml modules")
+                    print("Here's a good place to start: https://www.google.com/search?q=Installing+python+module+within+code ")
 input("Press any key to continue")
+
+
