117816e5358061cffe8168a66be8d44da3374e9d
e-sim-python <71469611+e-sim-python@users.noreply.github.com>
2020-09-29 00:51:40 +0300

    Update _bot_functions.py

--- Fight/_bot_functions.py
+++ Fight/_bot_functions.py
@@ -2,4 +2,4 @@ from sys import path
 from os.path import dirname
 
 path.append(dirname(path[0]))
-from Help_functions._bot_functions import _fighting, _random_sleep, _get_battle_id, _fix_product_name
+from Help_functions._bot_functions import *
