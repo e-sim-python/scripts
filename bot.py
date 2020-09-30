cf8f1009e1b71eadecf4e93e1995f732e48b6b31
e-sim-python <71469611+e-sim-python@users.noreply.github.com>
2020-09-30 05:11:27 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from Time_saver import *
 from login import get_nick_and_pw
 
 
-bot = commands.Bot(command_prefix = ".", case_insensitive=True)
+bot = commands.Bot(command_prefix=".", case_insensitive=True)
 YOUR_SECRET_TOKEN = input("Your discord token (help - https://www.writebots.com/discord-bot-token/): ")
 
 sessions = defaultdict(lambda: "")
@@ -35,7 +35,6 @@ def add_docs_for(other_func):
         func.__doc__ = other_func.__doc__ + "\n\n" + func.__doc__
         return func
     return dec
-
 
 @bot.command()
 @add_docs_for(accept_contract.accept_contract)
@@ -74,6 +73,259 @@ async def buff(ctx, buffs_names, *, nick):
         session = sessions[server]
         sessions[server] = buff.buffs(server, buffs_names, session)
         
+@bot.command()
+@add_docs_for(buy_cc.cc)
+async def cc(ctx, country_id, max_price, buy_amount, *, nick):
+    server = ctx.channel.name
+    if nick.lower() == MY_NICKS[server].lower():
+        session = sessions[server]
+        sessions[server] = buy_cc.cc(server, country_id, max_price, buy_amount, session)
+
+@bot.command()
+@add_docs_for(buy_product.products)
+async def buy(ctx, product, amount, *, nick):
+    server = ctx.channel.name
+    if nick.lower() == MY_NICKS[server].lower():
+        session = sessions[server]
+        sessions[server] = buy_product.products(server, product, amount, session)
+
+@bot.command()
+@add_docs_for(candidate.candidate)
+async def candidate(ctx, *, nick):
+    server = ctx.channel.name
+    if nick.lower() == MY_NICKS[server].lower():
+        session = sessions[server]
+        sessions[server] = candidate.candidate(server, session)
+
+@bot.command()
+@add_docs_for(change_avatar.avatar)
+async def avatar(ctx, *, nick):
+    """
+    If you don't want the default img, write like that:
+    `.avatar https://picsum.photos/150, Your Nick` (with a comma)"""
+    server = ctx.channel.name
+    if "," in nick:
+        imgURL, nick = nick.split(",")
+    else:
+        imgURL = "https://source.unsplash.com/random"
+    if nick.strip().lower() == MY_NICKS[server].lower():
+        session = sessions[server]
+        sessions[server] = change_avatar.avatar(server, imgURL.strip(), session)
+
+@bot.command()
+async def comment(ctx, shout_or_article_link, body, *, nick):
+    """
+    - `body` parameter MUST be within quotes.
+    - You can find shout link by clicking F12 in the page where you see the shout."""
+    server = ctx.channel.name
+    if nick.lower() == MY_NICKS[server].lower():
+        session = sessions[server]
+        if "article" in shout_or_article_link:
+            sessions[server] = comment_article.comment_article(server, shout_or_article_link.split("?id=")[1].split("&")[0], body, session)
+        elif "shout" in shout_or_article_link:
+            sessions[server] = comment_shout.comment_shout(server, shout_or_article_link.split("?id=")[1].split("&")[0], body, session)
+        else:
+            await ctx.send("Please provide a valid article/shout link.")
+
+@bot.command()
+@add_docs_for(donate_eqs.donate_eqs)
+async def donate(ctx, ids, receiver_id, *, nick):
+    """`ids` MUST be separated by a comma, and without spaces (or with spaces, but within quotes)"""
+    server = ctx.channel.name
+    if nick.lower() == MY_NICKS[server].lower():
+        session = sessions[server]
+        sessions[server] = donate_eqs.donate_eqs(server, ids, receiver_id, session)
+
+@bot.command()
+@add_docs_for(eqs.eqs)
+async def eq(ctx, *, nick):
+    server = ctx.channel.name
+    if nick.lower() == MY_NICKS[server].lower():
+        session = sessions[server]
+        sessions[server] = eqs.eqs(server, session)
+        # to do: aliases=["storage", "inventory"] (show all storage).
+
+@bot.command()
+@add_docs_for(fly.fly)
+async def Fly(ctx, region_link_or_id, ticket_quality, *, nick):
+    """Default: Q5 tickets"""
+    server = ctx.channel.name
+    try:
+        int(ticket_quality)
+    except:
+        nick = ticket_quality + " " + nick
+        ticket_quality = 5
+    if nick.lower() == MY_NICKS[server].lower():
+        session = sessions[server]
+        sessions[server] = fly.fly(server, region_link_or_id, ticket_quality, session)
+
+@bot.command()
+@add_docs_for(job.job)
+async def Job(ctx, *, nick):
+    server = ctx.channel.name
+    if nick.lower() == MY_NICKS[server].lower():
+        session = sessions[server]
+        sessions[server] = job.job(server, session)
+
+@bot.command(aliases=["dow", "mpp"])
+@add_docs_for(mpp_dow_attack.mpp_dow_attack)
+async def attack(ctx, ID, delay_or_battle_link, *, nick):
+    """`delay_or_battle_link` - optional"""
+    server = ctx.channel.name
+    if ".e-sim.org/battle.html?id=" not in delay_or_battle_link:
+        try:
+            int(delay_or_battle_link)
+        except:
+            nick = delay_or_battle_link + " " + nick
+            delay_or_battle_link = ""
+    if nick.lower() == MY_NICKS[server].lower():
+        session = sessions[server]
+        sessions[server] = mpp_dow_attack.mpp_dow_attack(server, ID, ctx.invoked_with.lower(), delay_or_battle_link, session)
+
+@bot.command()
+@add_docs_for(place_building.building)
+async def build(ctx, regionId, quality, Round, *, nick):
+    """`quality` = building quality (if you want to build an hospital instead, write like that: `5-hospital`)"""
+    server = ctx.channel.name
+    if nick.lower() == MY_NICKS[server].lower():
+        session = sessions[server]
+        sessions[server] = place_building.building(server, regionId, quality, Round, session)
+
+@bot.command()
+@add_docs_for(read.read)
+async def Read(ctx, *, nick):
+    server = ctx.channel.name
+    if nick.lower() == MY_NICKS[server].lower():
+        session = sessions[server]
+        sessions[server] = read.read(server, session)
+
+@bot.command()
+@add_docs_for(register.register)
+async def Register(ctx, lan, countryId, password, *, nick):
+    server = ctx.channel.name
+    if nick.lower() == MY_NICKS[server].lower():
+        session = sessions[server]
+        sessions[server] = register.register(server, nick, password, lan, countryId, session)
+
+@bot.command()
+@add_docs_for(report_citizen.report)
+async def report(ctx, target_id, report_reason, *, nick):
+    """`report_reason` MUST be within quotes"""
+    server = ctx.channel.name
+    if nick.lower() == MY_NICKS[server].lower():
+        session = sessions[server]
+        sessions[server] = report_citizen.report(server, target_id, report_reason, session)
+
+@bot.command(aliases=["upgrade"])
+@add_docs_for(reshuffle_or_upgrade.reshuffle_or_upgrade)
+async def reshuffle(ctx, eq_id_or_link, parameter, *, nick):
+    """`parameter` - it's recommended to copy and paste, but you can also write first/last"""
+    server = ctx.channel.name
+    if nick.lower() == MY_NICKS[server].lower():
+        session = sessions[server]
+        sessions[server] = reshuffle_or_upgrade.reshuffle_or_upgrade(server, ctx.invoked_with.lower(), eq_id_or_link, parameter, session)
+
+@bot.command()
+@add_docs_for(rw.rw)
+async def RW(ctx, region_id_or_link, ticket_quality, *, nick):
+    server = ctx.channel.name
+    try:
+        int(ticket_quality)
+    except:
+        nick = ticket_quality + " " + nick
+        ticket_quality = 5
+    if nick.lower() == MY_NICKS[server].lower():
+        session = sessions[server]
+        sessions[server] = rw.rw(server, region_id_or_link, ticket_quality, session)
+
+@bot.command()
+@add_docs_for(sell_eqs.sell_eqs)
+async def sell(ctx, ids, price, hours, *, nick):
+    """`ids` MUST be separated by a comma, and without spaces (or with spaces, but within quotes)"""
+    server = ctx.channel.name
+    if nick.lower() == MY_NICKS[server].lower():
+        session = sessions[server]
+        sessions[server] = sell_eqs.sell_eqs(server, ids, price, hours, session)
+
+@bot.command(aliases=["MU"])
+@add_docs_for(send_application.citizenship_or_mu_application)
+async def citizenship(ctx, country_or_mu_id, *, nick):
+    server = ctx.channel.name
+    if nick.lower() == MY_NICKS[server].lower():
+        session = sessions[server]
+        sessions[server] = send_application.citizenship_or_mu_application(server, country_or_mu_id, ctx.invoked_with.lower(), session)
+
+@bot.command()
+@add_docs_for(send_msg.send_msg)
+async def msg(ctx, receiver_name, title, body, *, nick):
+    """If any arg (receiverName, title or body) containing more than 1 word - it must be within quotes"""
+    server = ctx.channel.name
+    if nick.lower() == MY_NICKS[server].lower():
+        session = sessions[server]
+        sessions[server] = send_msg.send_msg(server, receiver_name, title, body, session)
+
+@bot.command()
+@add_docs_for(shout.shout)
+async def Shout(ctx, shout_body, *, nick):
+    """`shout_body` MUST be within quotes"""
+    server = ctx.channel.name
+    if nick.lower() == MY_NICKS[server].lower():
+        session = sessions[server]
+        sessions[server] = shout.shout(server, shout_body, session)
+
+@bot.command(aliases=["vote", "Vote_shout"])
+async def Sub(ctx, id, *, nick):
+    server = ctx.channel.name
+    if nick.lower() == MY_NICKS[server].lower():
+        session = sessions[server]
+        if ctx.invoked_with.lower() == "sub":
+            sessions[server] = sub.sub(server, id, session)
+        elif ctx.invoked_with.lower() == "vote":
+            sessions[server] = vote_article.article(server, id, session)
+        elif ctx.invoked_with.lower() == "vote_shout":
+            sessions[server] = vote_shout.vote_shout(server, id, session)
+
+@bot.command(aliases=["gift"])
+@add_docs_for(use.use)
+async def food(ctx, quality, *, nick):
+    """Default: Q5"""
+    server = ctx.channel.name
+    try:
+        int(quality)
+    except:
+        nick = quality + " " + nick
+        quality = 5
+    if nick.lower() == MY_NICKS[server].lower():
+        session = sessions[server]
+        sessions[server] = use.use(server, ctx.invoked_with.lower(), quality, session)
+
+@bot.command()
+@add_docs_for(vote_elections.elect)
+async def elect(ctx, your_candidate, *, nick):
+    """If `your_candidate` containing more than 1 word - it must be within quotes"""
+    server = ctx.channel.name
+    if nick.lower() == MY_NICKS[server].lower():
+        session = sessions[server]
+        sessions[server] = vote_elections.elect(server, your_candidate, session)
+
+@bot.command()
+@add_docs_for(vote_law.law)
+async def law(ctx, link_or_id, your_vote, *, nick):    
+    server = ctx.channel.name
+    if nick.lower() == MY_NICKS[server].lower():
+        session = sessions[server]
+        sessions[server] = vote_law.law(server, link_or_id, your_vote, session)
+
+@bot.command(aliases=["unwear"])
+async def wear(ctx, ids, *, nick):
+    """
+    Wear/take off specific EQ IDs.
+    `ids` MUST be separated by a comma, and without spaces (or with spaces, but within quotes)"""
+    server = ctx.channel.name
+    if nick.lower() == MY_NICKS[server].lower():
+        session = sessions[server]
+        sessions[server] = wear_unwear.wear_unwear(server, ids, session)
+
 
 @bot.event                                          
 async def on_message(message):
@@ -82,10 +334,10 @@ async def on_message(message):
         f = StringIO()
         with redirect_stdout(f):
             await bot.invoke(ctx)
-            
-        if f.getvalue():
+        output = f.getvalue()    
+        if output:
             # Sending the output (all prints) to your channel.
-            await ctx.send(f.getvalue())
+            await ctx.send(output[:2000])
         f.close()
 
         
