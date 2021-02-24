53d3f58f5d74ee0aaecf9e2421c28d6f1cd5883d
e-sim-python <71469611+e-sim-python@users.noreply.github.com>
2021-02-24 23:07:21 +0200

    Update mu_inventory.py

--- Basic/mu_inventory.py
+++ Basic/mu_inventory.py
@@ -3,52 +3,44 @@ import asyncio
 import __init__
 from login import get_content
 
+def import_tabulate():
+    try:
+        from tabulate import tabulate
+    except:
+        try:
+            import subprocess
+            subprocess.call(['pip', 'install', "tabulate"])
+            from tabulate import tabulate
+        except:
+            print(f"Couldnt Install the required Package, please type this in your CMD (command prompt):\n\n "
+                  f"pip install tabulate")
+            input("press enter to exit...")
+            return
+import_tabulate()
 
-async def MU_Inventory(ctx, server):
+
+async def MU_Inventory(server, ctx=None):
     """
     shows all of your in-game Military Unit inventory.
     """
     URL = f"https://{server}.e-sim.org/"
     tree = await get_content(f"{URL}militaryUnitStorage.html", login_first=True)
     container_1 = tree.xpath("//div[@class='storage']")
-    quantity = list()
-    for item in container_1:
-        quantity.append(item.xpath("div[1]/text()")[0].strip())
-    Products = list()
+    quantity = [item.xpath("div[1]/text()")[0].strip() for item in container_1]
+    products = list()
     for item in container_1:
         name = item.xpath("div[2]/img/@src")[0].replace("//cdn.e-sim.org//img/productIcons/", "").replace(".png", "")
         if name.lower() in ["iron", "grain", "diamonds", "oil", "stone", "wood"]:
             quality = ""
         else:
-            quality = item.xpath("div[2]/img/@src")[1].replace("//cdn.e-sim.org//img/productIcons/", "").replace(".png",
-                                                                                                                 "")
-        if quality:
-            Products.append(f"{quality.title()} {name}")
-        else:
-            Products.append(f"{name}")
+            quality = item.xpath("div[2]/img/@src")[1].replace(
+                "//cdn.e-sim.org//img/productIcons/", "").replace(".png", "")
+        products.append(f"{quality.title()} {name}" if quality else f"{name}")
     if ctx is None:
-        try:
-            from tabulate import tabulate
-        except:
-            def method4(package):
-                import subprocess
-                subprocess.call(['pip', 'install', package])
-                import tabulate
-
-            try:
-                method4("tabulate")
-            except:
-                print(f"Couldnt Install the required Package, please type this in your CMD (command prompt):\n\n "
-                      f"pip install tabulate")
-                input("press enter to exit...")
-                exit()
-            input(f"Installed tabulate package, restart the file, press enter to exit...")
-            exit()
-        Inventory = tabulate(zip(Products, quantity), headers=["Product", "Quantity"], tablefmt='grid',
-                             colalign=("left", "center"), showindex=False)
-        print(Inventory)
+        print(tabulate(zip(products, quantity), headers=["Product", "Quantity"],
+                       tablefmt='grid', colalign=("left", "center"), showindex=False))
     else:
-        return Products, quantity
+        return products, quantity
 
 
 if __name__ == "__main__":
@@ -56,5 +48,5 @@ if __name__ == "__main__":
     server = input("Server: ")
     loop = asyncio.get_event_loop()
     loop.run_until_complete(
-        MU_Inventory(ctx=None, server=server))
+        MU_Inventory(server))
     input("Press any key to continue")
