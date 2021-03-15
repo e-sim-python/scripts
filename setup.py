e13193f4912db095dbd91bc57640858d120552ff
Random-guy-on-internet <ali56341309@gmail.com>
2021-03-15 17:39:35 +0330

    version-bump

---
+++ setup.py
@@ -0,0 +1,31 @@
+import __init__
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="eSim",
+    version= "2.6.0",
+    author="e-sim-python",
+    description="E-sim python library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/e-sim-python/scripts",
+    packages=setuptools.find_packages(),
+    install_requires=["lxml", "discord.py", "pytz"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL-3.0 License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
+"""
+Usage:
+$ python -m build
+$ python -m twine upload dist/*
+username: __token__
+password: {my token}
+"""
